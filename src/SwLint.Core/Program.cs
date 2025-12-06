d0da9cce2d4705a8bb5ed8e05aac845c06ab9063
Ashton Dudley <adudley@dal.ca>
2025-12-06 00:48:31 -0400

    Added data for testing

--- src/SwLint.Core/Program.cs
+++ src/SwLint.Core/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
 Console.WriteLine("Hello, World!");
+
+
+string[] partNumbers = [ "1298-673-4192", "A08Z-931-468a",
+                      "_A90-123-129X", "12345-KKA-1230",
+                      "0919-2893-1256" ];
+string pattern = @"^[A-Z0-9]\d{2}[A-Z0-9](-\d{3}){2}[A-Z0-9]$";
+foreach (string partNumber in partNumbers)
+    try
+    {
+        bool isMatch = Regex.IsMatch(partNumber, pattern, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(500));
+        Console.WriteLine($"{partNumber} {(isMatch ? "is" : "is not")} a valid part number.");
+    }
+    catch (RegexMatchTimeoutException e)
+    {
+        Console.WriteLine($"Timeout after {e.MatchTimeout} seconds matching {e.Input}.");
+    }
+
+// The example displays the following output:
+//       1298-673-4192 is a valid part number.
+//       A08Z-931-468a is a valid part number.
+//       _A90-123-129X is not a valid part number.
+//       12345-KKA-1230 is not a valid part number.
+//       0919-2893-1256 is not a valid part number.
